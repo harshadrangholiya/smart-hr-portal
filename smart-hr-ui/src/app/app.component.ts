e036c67957c76701f44b2b75f59079e9a5d1036e
harshadrangholiya <harshadrangholiya34@gmail.com>
2025-06-22 14:21:52 +0530

    add admin dashboard code

--- smart-hr-ui/src/app/app.component.ts
+++ smart-hr-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smart-hr-ui';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showHeader = !['/auth/login', '/auth/register'].includes(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/auth/login']);
+  }
 }
